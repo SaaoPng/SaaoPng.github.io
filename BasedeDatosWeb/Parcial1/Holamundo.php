1fa6c3bae96d56d28168d03d3a97c1376c9dc0e2
Shiadani Andrade <159402033+SaaoPng@users.noreply.github.com>
2024-08-30 18:46:14 -0700

    Add files via upload

---
+++ BasedeDatosWeb/Parcial1/Holamundo.php
@@ -0,0 +1,19 @@
+<?php
+
+    $nombre = "Luis";
+    $numero = 4;
+    $numero2 = "4";
+
+    $total = $numero + $numero2;
+    
+    echo "Total = " . $total;
+
+    echo "<h1> Hola mundo </h1>";
+
+    for($i=0; $i<10; $i++){
+        echo $i . "<br>";
+    }
+
+    echo "<h1 style='color: gray;'> Andrade Ortiz Shiadani Alitzel 5AVP </h1>";
+
+?>
